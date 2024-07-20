aa2d0b2e86bbca3d6b7395aa44a54360df485df2
Mario Salazar <mariosalazar.ms.10@gmail.com>
2024-07-20 02:51:25 -0500

    Upload URL backend

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 //export const API_URL = import.meta.env.VITE_API_URL || "https://backend-auth-node.vercel.app/api";
-//export const API_URL = "https://backend-auth-node.vercel.app";
-export const API_URL = "http://localhost:4000";
+export const API_URL = "https://backend-auth-node.vercel.app";
+//export const API_URL = "http://localhost:4000";
 //export const API_URL = "https://backend-auth-node.vercel.app/api";
